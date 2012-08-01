7ca56ded051a5b9d34b6059ba7d59636022863b5
Kohei Takahashi <flast@flast.jp>
2012-08-01 23:14:33 +0900

    rename/move user level interface

--- packetix/packetix.php
+++ packetix/packetix.php
@@ -1,19 +1,22 @@
-<?php namespace PacketiX;
+<?php
+namespace PacketiX {
 require_once('client/readonly.php');
 
 class VPNException extends \Exception {}
+}
 
+namespace {
 // Use TCP/443 as default connection port.
-function create($host, $port = 443, $hub = false) {
+function packetix_readonly_client($host, $port = 443, $hub = false) {
   if (array_search('ssl', stream_get_transports()) == false) {
-    throw new VPNException('PacketiX VPN requires SSL connectivity');
+    throw new \PacketiX\VPNException('PacketiX VPN requires SSL connectivity');
   }
 
-  $client = new Client\Readonly($host, $port);
+  $client = new \PacketiX\Client\Readonly($host, $port);
   if ($hub) {
     $client->hub($hub);
   }
 
   return $client;
 }
-
+}
